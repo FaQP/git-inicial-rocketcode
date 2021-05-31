fdfe11d451490036576f25cfb2b6b4983a261dcd
FaQP <facundopietra74t6@gmail.com>
2021-05-31 12:31:15 -0300

    Base Page

---
+++ js/mb.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var elems = document.getElementById("materialboxed");
+    var options = "inDuration: 600, outDuration: 900";
+    var instances = M.Materialbox.init(elems, options);
+});
